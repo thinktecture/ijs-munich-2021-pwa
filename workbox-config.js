ad50e0879094ddace9ec18b6e27d49efce7adb59
Christian Liebel <christian.liebel@thinktecture.com>
2021-10-29 12:02:15 +0200

    Lab #9

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "src/",
   "globPatterns": [
-    "**/*.{html,js,webmanifest,css}"
+    "**/*.{html,js,png,webmanifest,css}"
   ],
   "swDest": "src/sw.js"
 };
