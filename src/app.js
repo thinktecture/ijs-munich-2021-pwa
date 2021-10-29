534041bb15af5ca52283a9e7c108bb3226955c1c
Christian Liebel <christian.liebel@thinktecture.com>
2021-10-29 09:55:01 +0200

    Lab #2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = 'black';
