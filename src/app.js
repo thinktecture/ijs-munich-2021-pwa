a9dd2665705f94887ec1f6ee397cb9abb5123b19
Christian Liebel <christian.liebel@thinktecture.com>
2021-10-29 10:28:35 +0200

    Lab #4 + #5

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { bresenhamLine } from "./helpers.js";
+
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d', {
     desynchronized: true
@@ -6,3 +8,22 @@ const ctx = canvas.getContext('2d', {
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = 'black';
+
+let previousPoint;
+
+canvas.addEventListener('pointerdown', event => {
+    previousPoint = { x: ~~event.offsetX, y: ~~event.offsetY };
+});
+canvas.addEventListener('pointermove', event => {
+    if (previousPoint) {
+        const currentPoint = { x: ~~event.offsetX, y: ~~event.offsetY };
+        for(let point of bresenhamLine(previousPoint.x, previousPoint.y,
+            currentPoint.x, currentPoint.y)) {
+            ctx.fillRect(point.x, point.y, 2, 2);
+        }
+        previousPoint = currentPoint;
+    }
+});
+canvas.addEventListener('pointerup', event => {
+    previousPoint = null;
+});
